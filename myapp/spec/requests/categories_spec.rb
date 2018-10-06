0a3c6a29cef394e6e15e10148b5cd1746aab0a87
Shingo Fukuyama <fukuyama012@gmail.com>
2018-10-07 00:52:19 +0900

    add category get index spec add category post spec

--- myapp/spec/requests/categories_spec.rb
+++ myapp/spec/requests/categories_spec.rb
@@ -11,11 +11,49 @@ RSpec.describe "Categories", type: :request do
       expect(response.status).to eq(200)
     end
 
-    it '情報を取得できる' do
+    it 'カテゴリ情報を取得できる' do
       json = JSON.parse(response.body)
       expect(json['id']).to eq(@category.id)
       expect(json['name']).to eq(@category.name)
     end
-    
+  end
+
+  describe "GET /api/v1/categories" do
+    before do
+      @category1 = create(:category, id: 1000, name: 'everyday routine')
+      @category2 = create(:category, id: 1001, name: 'weekly routine')
+      get "/api/v1/categories"
+    end
+
+    it '200 OK が返ってくる' do
+      expect(response.status).to eq(200)
+    end
+
+    it 'カテゴリ一覧情報を取得できる' do
+      json = JSON.parse(response.body)
+      expect(json[0]['name']).to eq('everyday routine')
+      expect(json[1]['name']).to eq('weekly routine')
+    end
+  end
+
+  describe 'POST /api/v1/categories' do
+    let(:path) { '/api/v1/categories' }
+
+    context '1. パラメータが正しいとき' do
+      before do
+        @params = { category: attributes_for(:category) }
+      end
+
+      it '201 Created が返ってくる' do
+        post path, params: @params
+        expect(response.status).to eq(201)
+      end
+
+      it 'Category が 1 増える' do
+        expect {
+          post path, params: @params
+        }.to change(Category, :count).by(1)
+      end
+    end
   end
 end
