f12b807362d7383b2e99dce025c8a21032ae0ef9
Shingo Fukuyama <fukuyama012@gmail.com>
2018-10-06 20:08:50 +0900

    add first categories rspec

--- myapp/spec/requests/categories_spec.rb
+++ myapp/spec/requests/categories_spec.rb
@@ -1,10 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe "Categories", type: :request do
-  describe "GET /categories" do
-    it "works! (now write some real specs)" do
-      get categories_path
-      expect(response).to have_http_status(200)
+  describe "GET /api/v1/categories/:id" do
+    before do
+      @category = create(:category)
+      get "/api/v1/categories/#{@category.id}"
     end
+
+    it '200 OK が返ってくる' do
+      expect(response.status).to eq(200)
+    end
+
+    it '情報を取得できる' do
+      json = JSON.parse(response.body)
+      expect(json['id']).to eq(@category.id)
+      expect(json['name']).to eq(@category.name)
+    end
+    
   end
 end
--- myapp/spec/requests/reminders_spec.rb
+++ myapp/spec/requests/reminders_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe "Reminders", type: :request do
   describe "GET /reminders" do
     it "works! (now write some real specs)" do
-      get reminders_path
-      expect(response).to have_http_status(200)
+      #get reminders_path
+      #expect(response).to have_http_status(200)
     end
   end
 end
