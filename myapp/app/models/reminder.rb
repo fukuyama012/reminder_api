4d21ef6975bc018cf26b0b287b4e041bffd944d1
Shingo Fukuyama <fukuyama012@gmail.com>
2018-10-07 19:36:34 +0900

    add reminder request spec  *POST

--- myapp/app/models/reminder.rb
+++ myapp/app/models/reminder.rb
@@ -1,3 +1,5 @@
 class Reminder < ApplicationRecord
   belongs_to :category
+  validates :notify, presence: true
+  validates :cycle_days, presence: true, numericality: { only_integer: true }
 end
--- myapp/spec/requests/reminders_spec.rb
+++ myapp/spec/requests/reminders_spec.rb
@@ -37,5 +37,88 @@ RSpec.describe "Reminders", type: :request do
       expect(json[0]['notify']).to eq('Wake up!')
       expect(json[1]['notify']).to eq('Sleep now!!')
     end
-  end 
+  end
+
+  describe 'POST /api/v1/reminders' do
+    let(:path) { '/api/v1/reminders' }
+
+    context '1-1. パラメータが正しいとき' do
+      before do
+        @category = create(:category)
+        @params = { reminder: attributes_for(:reminder, category_id: @category.id) }
+      end
+
+      it '201 Created が返ってくる' do
+        post path, params: @params
+        expect(response.status).to eq(201)
+      end
+
+      it '同じnotifyを登録しても201 Created が返ってくる' do
+        post path, params: @params
+        post path, params: @params
+        expect(response.status).to eq(201)
+      end
+
+      it 'Reminder が 1 増える' do
+        expect {
+          post path, params: @params
+        }.to change(Reminder, :count).by(1)
+      end
+    end
+
+    context '1-2. notifyが入っていないとき' do
+      before do
+        @category = create(:category)
+        @params = { reminder: attributes_for(:reminder, category_id: @category.id, notify: '') }
+      end
+
+      it '422 Unprocessable Entity が返ってくる' do
+        post path, params: @params
+        expect(response.status).to eq(422)
+      end
+
+      it 'Reminderが増減しない' do
+        expect {
+          post path, params: @params
+        }.not_to change(Reminder, :count)
+      end
+    end
+
+    context '1-3. cycle_days入っていないとき' do
+      before do
+        @category = create(:category)
+        @params = { reminder: attributes_for(:reminder, category_id: @category.id, cycle_days: '') }
+      end
+
+      it '422 Unprocessable Entity が返ってくる' do
+        post path, params: @params
+        expect(response.status).to eq(422)
+      end
+
+      it 'Reminderが増減しない' do
+        expect {
+          post path, params: @params
+        }.not_to change(Reminder, :count)
+      end
+    end
+
+    context '1-4. cycle_daysが整数でないとき' do
+      before do
+        @category = create(:category)
+        @params = { reminder: attributes_for(:reminder, category_id: @category.id, cycle_days: 'a') }
+      end
+
+      it '422 Unprocessable Entity が返ってくる' do
+        post path, params: @params
+        expect(response.status).to eq(422)
+      end
+
+      it 'Reminderが増減しない' do
+        expect {
+          post path, params: @params
+        }.not_to change(Reminder, :count)
+      end
+    end
+  end
+
 end
