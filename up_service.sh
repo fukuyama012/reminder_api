942f2401fa1f4998a40d84423e792f6ec9cc4d63
Shingo Fukuyama <fukuyama012@gmail.com>
2018-12-01 21:43:07 +0900

    add front end build processing to docker-compose

--- up_service.sh
+++ up_service.sh
@@ -2,5 +2,7 @@
 
 docker-compose run --rm app rails db:create
 docker-compose run --rm app rails db:migrate
+# build front end client
+docker-compose run --rm --workdir="/myapp/client" app yarn build
 
 docker-compose up -d
