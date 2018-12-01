798daa83bec4282872674c62df9b7a909d2fa8bb
Shingo Fukuyama <fukuyama012@gmail.com>
2018-12-01 16:16:31 +0900

    setup dev API Client (React, Typescript)

---
+++ myapp/client/src/index.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+ReactDOM.render(
+    <div style={{border:'1px solid', height:'50px'}}/>,
+    document.getElementById('root')
+);
